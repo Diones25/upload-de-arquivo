6d228ca3b456f5a53ca1df9389a93a16c5d40fef
Diones Alves <pereiradiones987@gmail.com>
2020-07-09 19:10:39 -0300

    Alteração no código para ser possível fazer upload do mesmo arquivo com nome diferente

--- receber.php
+++ receber.php
@@ -3,7 +3,9 @@
     
     if(isset($arquivo['tmp_name']) && empty($arquivo['tmp_name']) == false){
 
-        move_uploaded_file($arquivo['tmp_name'], "arquivos/".$arquivo['name']);
+        $nomearquivo = md5(time().rand(0,99)).'png';
+
+        move_uploaded_file($arquivo['tmp_name'], "arquivos/".$nomearquivo);
         echo "Arquivo enviado com sucesso!";
 
     }
